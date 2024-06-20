b2a032f3a3b595631380034d51079508b9ca2403
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-06-20 15:50:48 +0500

    Fixing bug with goind back to print ticket page

--- src/views/IIN_Input.vue
+++ src/views/IIN_Input.vue
@@ -144,7 +144,7 @@ export default {
       }, idleTimeout);
     },
     goBack() {
-      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+      this.$router.push('/')
     }
   },
   mounted() {
--- src/views/PrintTicket.vue
+++ src/views/PrintTicket.vue
@@ -1,6 +1,6 @@
 <script>
-const iin_req = import.meta.env.VITE_SERVER_INN_REQUIRED
-const phone_req = import.meta.env.VITE_SERVER_PHONE_REQUIRED
+// const iin_req = import.meta.env.VITE_SERVER_INN_REQUIRED
+// const phone_req = import.meta.env.VITE_SERVER_PHONE_REQUIRED
 import axios from 'axios'
 import { useStateStore } from '../store'
 // import { BRANCH_ID } from '../constants'
@@ -16,16 +16,12 @@ export default {
   },
   methods: {
     getStart() {
-      if (iin_req === 'true') {
-        this.$router.push('/iin')
-      } else if (phone_req === 'true') {
-        this.$router.push('/phone')
-      } else this.$router.push('/')
+      this.$router.push('/')
     },
     async print() {
             try {
                 const ticketBody = this.stateStore.get_ticket_body
-                const response = await axios.post(`http://${SERVER_HOST}:${SERVER_PORT}/api/v1/services/print`, {
+                 await axios.post(`http://${SERVER_HOST}:${SERVER_PORT}/api/v1/services/print`, {
                     local: this.stateStore.get_lang,
                     // local: 'ru',
                     soapBody: ticketBody,
